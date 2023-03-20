26d008bea4fa5a598735c62dac9c855df1751988
snowfixx <128341779+snowfixx@users.noreply.github.com>
2023-03-20 11:10:05 +0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -48,6 +48,7 @@ echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
+gotp tp to npc
 
 :findJavaFromJavaHome
  set JAVA_HOME=%JAVA_HOME:"=%
@@ -64,9 +65,9 @@ echo location of your Java installation.
 goto fail
 
 :execute
-@rem Setup the command line
+ @rem Setup the command line
 
- set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
 
 @rem Execute Gradle
@@ -77,13 +78,13 @@ goto fail
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+ rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+ rem the _cmd.exe /c_ return code!
+ if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
 
  :omega
-:strat fail
+:strat 
