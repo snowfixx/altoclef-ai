045603bc61700e37270a83b171ebff2a83905e8b
snowfixx <128341779+snowfixx@users.noreply.github.com>
2023-03-20 11:14:11 +0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -48,7 +48,6 @@ echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
-gotp tp to npc
 
 :findJavaFromJavaHome
  set JAVA_HOME=%JAVA_HOME:"=%
@@ -61,6 +60,10 @@ echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
+
+echo eps
+echo hack
+echo.
 
 goto fail
 
